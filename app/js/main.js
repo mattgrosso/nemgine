e0536873b9c037266efe13f4f7c9035faaaccd1b
Matt Grosso <mattgrosso@gmail.com>
2020-02-01 16:37:01 -0500

    Adds firebase database and logging

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,13 @@
 /*jshint esversion: 6 */
-console.error("This is a flag");
+const db = firebase.firestore();
+const log = db.collection("log");
+
+const input = document.querySelector(".input");
+
+input.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    log.doc(new Date().toGMTString()).set({
+      message: input.value
+    })
+  }
+});
